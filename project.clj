c15a0e766012cc376e04b74d893cc7ed57d3f6dd
sooheon <tngjs0@gmail.com>
2014-11-24 01:49:47 +0900

    Looping is recursion is done

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
 (defproject looping-is-recursion "1.0.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [iloveponies.tests/looping-is-recursion "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
+  :profiles {:dev {:plugins [[cider/cider-nrepl "0.8.1"][lein-midje "3.1.1"]]}})
--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -1,3 +1,5 @@
+;; http://iloveponies.github.io/120-hour-epic-sax-marathon/looping-is-recursion.html
+
 (ns looping-is-recursion)
 
 (defn recursive-factorial [n]
@@ -21,32 +23,80 @@
     (helper 1 number)))
 
 (defn power [base exp]
-  (let [helper (fn [acc b exp]
+  (let [helper (fn [acc exp]
                  (if (zero? exp)
                    acc
-                   (recur (* acc b) b (dec exp))))]
-    (helper 1 base exp)))
+                   (recur (* acc base) (dec exp))))]
+    (helper 1 exp)))
 
 (defn last-element [a-seq]
-  )
-
-(defn plus [num numb]
-  (+ num numb))
+  (let [helper (fn [last a-seq]
+                 (if (empty? a-seq)
+                   last
+                   (recur (first a-seq) (rest a-seq))))]
+    (helper nil a-seq)))
 
 (defn seq= [seq1 seq2]
-  )
+  (let [helper (fn [passed? s1 s2]
+                 (cond
+                  (= false passed?) false
+                  (and (empty? s1) (empty? s2)) true
+                  (or (empty? s1) (empty? s2)) false
+                  :else (recur (= (first s1) (first s2)) (rest s1) (rest s2))))]
+    (helper true seq1 seq2)))
+
+(defn loopy-factorial [down-from]
+  (loop [acc 1
+         n down-from]
+    (if (zero? n)
+      acc
+      (recur (* acc n) (dec n)))))
 
 (defn find-first-index [pred a-seq]
-  )
+  (loop [idx 0
+         seq a-seq]
+    (cond
+     (empty? seq) nil
+     (pred (first seq)) idx
+     :else (recur (inc idx) (rest seq)))))
 
 (defn avg [a-seq]
-  )
+  (loop [count 0
+         sum 0
+         seq a-seq]
+    (cond
+     (empty? a-seq) nil
+     (empty? seq) (/ sum count)
+     :else (recur (inc count) (+ sum (first seq)) (rest seq)))))
 
 (defn parity [a-seq]
-  )
+  (let [toggle (fn [a-set elem]
+                 (if (contains? a-set elem)
+                   (disj a-set elem)
+                   (conj a-set elem)))]
+    (loop [par-set #{}
+           seq a-seq]
+      (if (empty? seq)
+        result
+        (recur (toggle par-set (first seq)) (rest seq))))))
 
 (defn fast-fibo [n]
-  )
+  (loop [fn-2 0
+         fn-1 1
+         idx (dec n)]
+    (cond
+     (zero? n) 0
+     (= n 1) 1
+     (zero? idx) fn-1
+     :else (recur fn-1 (+ fn-2 fn-1) (dec idx)))))
 
 (defn cut-at-repetition [a-seq]
-  )
+  (loop [seq a-seq
+         elems #{}
+         cut []]
+    (cond
+     (empty? seq) cut
+     (contains? elems (first seq)) cut
+     :else (recur (rest seq) 
+                  (conj elems (first seq)) 
+                  (conj cut (first seq)))))) 
